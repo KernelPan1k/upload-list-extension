880851226649d2b6446a252009b732409df497f0
kp <contact@kernel-panik.me>
2021-11-20 22:31:58 +0100

    Initial commit

---
+++ generate.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+payload_name="shell"
+
+for char in '%20' '%0a' '%00' '%0d0a' '/' '.\\' '.' '…' ':'; do
+    for ext in '.php' '.php2' '.php3' '.php4' '.php5' '.php6' '.php7' '.phps' '.phps' '.pht' '.phtm' '.phtml' '.pgif' '.shtml' '.htaccess' '.phar' '.inc'; do
+        echo "${payload_name}${ext}" >> wordlist.txt
+        echo "${payload_name}${ext^^}" >> wordlist.txt
+        echo "${payload_name}$(echo ${ext} | sed 's/./\U&/3')" >> wordlist.txt
+        echo "${payload_name}${char}${ext}.jpg" >> wordlist.txt
+        echo "${payload_name}${char}${ext^^}.jpg" >> wordlist.txt
+        echo "${payload_name}${char}$(echo ${ext} | sed 's/./\U&/3').jpg" >> wordlist.txt
+        echo "${payload_name}${char}${ext}.jpg" >> wordlist.txt
+        echo "${payload_name}${char}${ext^^}.jpg" >> wordlist.txt
+        echo "${payload_name}${char}$(echo ${ext} | sed 's/./\U&/3').jpg" >> wordlist.txt
+        echo "${payload_name}${ext}${char}.jpg" >> wordlist.txt
+        echo "${payload_name}${ext^^}${char}.jpg" >> wordlist.txt
+        echo "${payload_name}$(echo ${ext} | sed 's/./\U&/3')${char}.jpg" >> wordlist.txt
+        echo "${payload_name}.jpg${char}${ext}" >> wordlist.txt
+        echo "${payload_name}.jpg${char}${ext^^}" >> wordlist.txt
+        echo "${payload_name}.jpg${char}$(echo ${ext} | sed 's/./\U&/3')" >> wordlist.txt
+        echo "${payload_name}.jpg${ext}${char}" >> wordlist.txt
+        echo "${payload_name}.jpg${ext^^}${char}" >> wordlist.txt
+        echo "${payload_name}.jpg$(echo ${ext} | sed 's/./\U&/3')${char}" >> wordlist.txt
+    done
+done
